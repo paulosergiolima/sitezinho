00b048a791d8c199215686516a548bd4ec47aba0
PauloLimaMeerkat <paulo.lima@meerkatcoding.com>
2025-03-18 18:29:36 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+
+# A very simple Flask Hello World app for you to get started with...
+from os import listdir
+import os
+from os.path import isfile, join
+from bson import BSON
+from flask import Flask, after_this_request, render_template, request
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    onlyfiles = [f for f in listdir("./static/images") if isfile(join("./static/images", f))]
+    return render_template('index.html', images = onlyfiles)
+
+@app.route('/vote', methods = ['POST'])
+def vote():
+    uri = f'mongodb://sitezinho:sitezinho@urna-shard-00-00.yj774.mongodb.net:27017,urna-shard-00-01.yj774.mongodb.net:27017,urna-shard-00-02.yj774.mongodb.net:27017/?replicaSet=atlas-y8c7zw-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=urna'
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    try:
+        client.admin.command('ping')
+        mydb = client["urna"]
+        votes = mydb["votes"]
+        json_request = request.json
+        ballot = {"Name": json_request[0], "Votes": request.json[1]}
+        print(ballot)
+        print(type(ballot))
+        x = votes.insert_one(ballot)
+        print(x)
+    except Exception as e:
+        print(e)
+    print(request.json)
+    client.close()
+    return "hi"
+
+    
+
---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+url = "localhost:8080/vote"
+
+function vote() {
+    username = prompt("Qual seu usário?")
+    let votes = new Array()
+    var inputs = document.querySelectorAll("input[type='checkbox']");
+    for(var i = 0; i < inputs.length; i++) {
+        if(inputs[i].checked === true) {
+            votes.push(inputs[i].name)
+
+        }
+    }
+    console.log(votes)
+    json_votes = JSON.stringify([username, votes])
+    console.log(json_votes)
+    fetch('http://127.0.0.1:5000/vote', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: json_votes
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Error:', error));
+      
+}
+async function fetchAsync (url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+  }
