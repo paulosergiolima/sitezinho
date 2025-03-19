8a8c95c12194129d97185b377be2e38383446406
PauloLimaMeerkat <cloverfieldor@gmail.com>
2025-03-19 14:33:11 -0300

    comments

--- app.py
+++ app.py
@@ -23,20 +23,17 @@ def vote():
     uri = f'mongodb://sitezinho:sitezinho@urna-shard-00-00.yj774.mongodb.net:27017,urna-shard-00-01.yj774.mongodb.net:27017,urna-shard-00-02.yj774.mongodb.net:27017/?replicaSet=atlas-y8c7zw-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=urna'
     client = MongoClient(uri, server_api=ServerApi('1'))
     try:
-        client.admin.command('ping')
         mydb = client["urna"]
         votes = mydb["votes"]
         json_request = request.json
         ballot = {"Name": json_request[0], "Votes": request.json[1]}
         print(ballot)
-        print(type(ballot))
         x = votes.insert_one(ballot)
         print(x)
     except Exception as e:
         print(e)
-    print(request.json)
     client.close()
-    return "hi"
+    return "Worked"
 
     
 
