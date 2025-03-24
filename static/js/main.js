07b9b0fa681ae1224a903215fdec93a48531d486
PauloLimaMeerkat <cloverfieldor@gmail.com>
2025-03-24 18:29:17 -0300

    votes on prod

--- static/js/main.js
+++ static/js/main.js
@@ -13,7 +13,7 @@ function vote() {
     console.log(votes)
     json_votes = JSON.stringify([username, votes])
     console.log(json_votes)
-    fetch('vote', {
+    fetch('https://kinipk.pythonanywhere/vote', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
