38f182e721b1ce6fab6fd9fdaaa167a5e5b71606
PauloLimaMeerkat <cloverfieldor@gmail.com>
2025-03-25 17:27:19 -0300

    Document name and single_vote

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,5 @@
 url = "localhost:8080/vote"
-
+single_vote = false
 function vote() {
     username = prompt("Qual seu usário?")
     let votes = new Array()
@@ -24,4 +24,23 @@ function vote() {
       .then(data => console.log(data))
       .catch(error => console.error('Error:', error));
       
+}
+
+
+if (single_vote == true) {
+  $("input:checkbox").on('click', function() {
+    // in the handler, 'this' refers to the box clicked on
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      // the name of the box is retrieved using the .attr() method
+      // as it is assumed and expected to be immutable
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      // the checked state of the group/box on the other hand will change
+      // and the current value is retrieved using .prop() method
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
+    }
+  });
 }
