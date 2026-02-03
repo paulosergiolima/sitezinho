0b36962392bf5f61ee7809e5cc54f0b871a586ed
paulosergiolima <cloverfieldor@gmail.com>
2026-02-03 11:46:06 -0300

    refactor: add jQuery as a dependency and update voting functionality to use jQuery for checkbox handling

--- sitezinho/static/js/main.js
+++ sitezinho/static/js/main.js
@@ -1,25 +1,4 @@
 // Global variables for vote configuration (set by template)
-let single_vote = false;
-
-
-
-if (single_vote == true) {
-  $("input:checkbox").on('click', function() {
-    // in the handler, 'this' refers to the box clicked on
-    var $box = $(this);
-    if ($box.is(":checked")) {
-      // the name of the box is retrieved using the .attr() method
-      // as it is assumed and expected to be immutable
-      var group = "input:checkbox[name='" + $box.attr("name") + "']";
-      // the checked state of the group/box on the other hand will change
-      // and the current value is retrieved using .prop() method
-      $(group).prop("checked", false);
-      $box.prop("checked", true);
-    } else {
-      $box.prop("checked", false);
-    }
-  });
-}
 
 // Initialize page and modal event listeners when page loads
 
--- sitezinho/static/js/voting.js
+++ sitezinho/static/js/voting.js
@@ -1,4 +1,5 @@
 import { clearAllCheckboxes } from "./utils.js";
+import {$} from "jquery";
 async function vote() {
   try {
     const username = prompt("Qual seu usuário?");
@@ -89,3 +90,23 @@ async function vote() {
 }
 var vote_button = document.getElementById("vote")
 vote_button.addEventListener('click', vote)
+
+let single_vote = window.VOTE_CONFIG?.single_vote || false;
+
+if (single_vote == true) {
+  $("input:checkbox").on('click', function() {
+    // in the handler, 'this' refers to the box clicked on
+    var $box = $(this);
+    if ($box.is(":checked")) {
+      // the name of the box is retrieved using the .attr() method
+      // as it is assumed and expected to be immutable
+      var group = "input:checkbox[name='" + $box.attr("name") + "']";
+      // the checked state of the group/box on the other hand will change
+      // and the current value is retrieved using .prop() method
+      $(group).prop("checked", false);
+      $box.prop("checked", true);
+    } else {
+      $box.prop("checked", false);
+    }
+  });
+}
